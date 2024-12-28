f67e69f507438aac232390f1ca6a222c3ef673ce
jaumeol <joliverlafont@gmail.com>
2024-12-28 08:23:52 +0100

    main -> master so it triggers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([]), // Add your routes here
-    
+    AppComponent
   ],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent]
